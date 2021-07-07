3948bc4e6813e2a21d6a75728b36c4cbda856051
Diana <aneroma@yahoo.com@git config --global user.name diana@>
2021-07-07 16:38:44 +0100

    Exercise 4 completed

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -1,10 +1,49 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
-const Demo = () => {
+const Demo = props => {
+  const list = props.modules.map((module, index) => {
+    return (
+      <tr key={index}>
+      DevOps
+      </tr>
+    );
+  });
   return (
-      <h1>TODO</h1>
+    <>
+      <h2>{props.course}</h2>
+      <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <td>DevOps</td>
+        <td>2</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>Enterprise Web Dev</td>
+        <td>3</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>Programming</td>
+        <td>2</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>Computer Systems</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+    </tbody >
+  </table>
+    </>
   );
 };
 
